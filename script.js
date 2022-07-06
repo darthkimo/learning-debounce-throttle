dc6d8be58c1de02e092c861baf2bd18fc6998012
Wei-Hsin Chen <weihsinc@google.com>
2022-07-06 15:18:55 +0800

    Add trottle

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const throttleText = document.getElementById('throttle');
 
 const updateDebounceText = debounce((text) => {
   debounceText.textContent = text;
-}, 250)
+})
+
+const updateThrottleText = throttle((text) => {
+  throttleText.textContent = text;
+})
 
 input.addEventListener('input', e => {
   defaultText.innerHTML = e.target.value;
   updateDebounceText(e.target.value);
+  updateThrottleText(e.target.value);
 });
 
 function debounce(cb, delay = 1000){
@@ -20,5 +25,38 @@ function debounce(cb, delay = 1000){
     timeout = setTimeout(()=>{
       cb(...args);
     }, delay);
+  }
+}
+
+/**
+ * 
+ * @param {*} cb 
+ * @param {number} delay 
+ * @returns 
+ */
+
+function throttle(cb, delay = 1000){
+  let shouldWait = false;
+  let waitingArgs;
+  const timeoutFunc = ()=>{
+    if (waitingArgs == null) {
+      shouldWait = false;
+    } else {
+      cb(...waitingArgs);
+      waitingArgs = null;
+      shouldWait = false;
+      setTimeout(timeoutFunc, delay);
+    }
+  }
+  
+  return (...args) => {
+    if(shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+
+    cb(...args);
+    shouldWait = true;
+    setTimeout(timeoutFunc, delay);
   }
 }
